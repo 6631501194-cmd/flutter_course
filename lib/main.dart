b4d23ecbe47ee4c3bf8fe22b3dac001c72e290bd
Zaw Linn Phyo <6631501194@lamduan.mfu.ac.th>
2025-09-16 21:45:34 +0700

    Add simple row column design

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course/week05/design1.dart';
 import 'package:flutter_course/week05/multiple_child_demo.dart';
 import 'package:flutter_course/week05/multiple_child_demo2.dart';
 import 'package:flutter_course/week05/single_child_demo.dart';
 
 void main() {
-  runApp(MaterialApp(home: MultipleChildDemo2()));
+  runApp(MaterialApp(home: Design1()));
 }

10c3d72f20a49f5f1fc6cf53725143271dbcce0b
Zaw Linn Phyo <6631501194@lamduan.mfu.ac.th>
2025-09-16 22:29:18 +0700

    Generate random number

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,9 @@ import 'package:flutter_course/week05/design1.dart';
 import 'package:flutter_course/week05/design2.dart';
 import 'package:flutter_course/week05/multiple_child_demo.dart';
 import 'package:flutter_course/week05/multiple_child_demo2.dart';
+import 'package:flutter_course/week05/random_demo.dart';
 import 'package:flutter_course/week05/single_child_demo.dart';
 
 void main() {
-  runApp(MaterialApp(home: Design2()));
+  runApp(MaterialApp(home: RandomDemo()));
 }

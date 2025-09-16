c597b565b88b1f6c45dd32a8547cf378053e38ed
Zaw Linn Phyo <6631501194@lamduan.mfu.ac.th>
2025-09-16 22:14:48 +0700

    Add new profile design

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_course/week05/design1.dart';
+import 'package:flutter_course/week05/design2.dart';
 import 'package:flutter_course/week05/multiple_child_demo.dart';
 import 'package:flutter_course/week05/multiple_child_demo2.dart';
 import 'package:flutter_course/week05/single_child_demo.dart';
 
 void main() {
-  runApp(MaterialApp(home: Design1()));
+  runApp(MaterialApp(home: Design2()));
 }

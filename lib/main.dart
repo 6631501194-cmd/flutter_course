550b19918ae9368a6063d721ba6cfcd98727fa13
Zaw Linn Phyo <6631501194@lamduan.mfu.ac.th>
2025-09-22 18:31:24 +0700

    Baking demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course/week06/baker_demo.dart';
 import 'package:flutter_course/week06/button_demo.dart';
 import 'package:flutter_course/week06/image_demo.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: ImageDemo()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: BakerDemo()));
 }

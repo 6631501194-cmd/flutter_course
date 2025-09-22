af3307c8f1f0c6c515a982990ad5bb0dd29f3c92
Zaw Linn Phyo <6631501194@lamduan.mfu.ac.th>
2025-09-22 20:56:35 +0700

    Add normal timer demo

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter_course/week06/baker_demo.dart';
 import 'package:flutter_course/week06/button_demo.dart';
 import 'package:flutter_course/week06/image_demo.dart';
 import 'package:flutter_course/week06/login_dynamic.dart';
+import 'package:flutter_course/week06/timer_deomo.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: LoginDynamic()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: TimerDeomo()));
 }

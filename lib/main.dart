175887eeb7ac0f1041050c5404f68a885796b48a
Zaw Linn Phyo <6631501194@lamduan.mfu.ac.th>
2025-10-13 21:43:53 +0700

    Add slider function

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'package:flutter_course/week07/cb_sw_demo.dart';
 import 'package:flutter_course/week07/dialog_demo.dart';
 import 'package:flutter_course/week07/dropdown.dart';
 import 'package:flutter_course/week07/radio_demo.dart';
+import 'package:flutter_course/week07/slider_demo.dart';
 
 
 
  void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: RadioDemo()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: SliderDemo()));
 }

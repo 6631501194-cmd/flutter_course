e5fd37400bda9f8f56dfb47465b3604e4e22d3da
Zaw Linn Phyo <6631501194@lamduan.mfu.ac.th>
2025-10-17 10:43:10 +0700

    add navigation with data

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course/week08/page%201.dart';
 import 'package:flutter_course/week08/pageview_demo.dart';
 import 'package:flutter_course/week08/scrollview_demo.dart';
 import 'package:flutter_course/week08/tab_demo.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: PageviewDemo()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Page1()));
 }

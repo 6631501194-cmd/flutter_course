d082e2d1e243b63da83d4eec161977b5e01fedc0
Zaw Linn Phyo <6631501194@lamduan.mfu.ac.th>
2025-10-17 10:16:32 +0700

    add pageview,tab, scrollview function

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course/week08/pageview_demo.dart';
+import 'package:flutter_course/week08/scrollview_demo.dart';
+import 'package:flutter_course/week08/tab_demo.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'My First App',
-            style: TextStyle(fontSize: 24, color: Colors.white),
-          ),
-          backgroundColor: Color.fromARGB(255, 255, 0, 242),
-        ),
-        body: Center(
-          child: Text(
-            'Hello world ',
-            style: TextStyle(fontSize: 32, color: const Color.fromARGB(255, 47, 18, 174)),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // Add action here
-          },
-          backgroundColor: Colors.blue,
-          child: Icon(Icons.add, color: Colors.white),
-        ),
-      ),
-    ),
-  );
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: PageviewDemo()));
 }
---
+++ lib/week08/tab_demo.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class TabDemo extends StatefulWidget {
+  const TabDemo({super.key});
+
+  @override
+  State<TabDemo> createState() => _TabDemoState();
+}
+
+class _TabDemoState extends State<TabDemo> {
+  String message = 'Home';
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(title: Text("Tab demo"),),
+        bottomNavigationBar: Container(
+          color: Colors.amber,
+          child: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home), text: message),
+              Tab(icon: Icon(Icons.train), text: 'Train'),
+              Tab(icon: Icon(Icons.bike_scooter), text: 'bike_scooter'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Container(color: Colors.yellow, child: Text(message)),
+            Container(color: Colors.green, child: Text("Train")),
+            Container(
+              alignment: Alignment.topCenter,
+              color: Colors.red,
+              child: FilledButton(
+                onPressed: () {
+                  setState(() {
+                    message = 'Hello';
+                  });
+                },
+                child: Text("update"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

9eb513389f0096a11f078a427f0053b6f4379044
Zaw Linn Phyo <6631501194@lamduan.mfu.ac.th>
2025-10-24 10:05:31 +0700

    Add Listview function

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_course/week09/drawer_demo.dart';
+import 'package:flutter_course/week09/listview_demo.dart';
 import 'package:flutter_course/week09/snackbar_demo.dart';
 
 
 
  void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: DrawerDemo()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: ListviewDemo()));
 }
 

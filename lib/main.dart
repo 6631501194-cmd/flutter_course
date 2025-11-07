92f94908e4ce210ddfcc0ae39806025a385cbaae
Zaw Linn Phyo <6631501194@lamduan.mfu.ac.th>
2025-11-07 08:11:23 +0700

    add function

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course/expenses/expense.dart';
+import 'package:flutter_course/expenses/login.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'My First App',
-            style: TextStyle(fontSize: 24, color: Colors.white),
-          ),
-          backgroundColor: Color.fromARGB(255, 255, 0, 242),
-        ),
-        body: Center(
-          child: Text(
-            'Hello world ',
-            style: TextStyle(fontSize: 32, color: const Color.fromARGB(255, 47, 18, 174)),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // Add action here
-          },
-          backgroundColor: Colors.blue,
-          child: Icon(Icons.add, color: Colors.white),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Expense(),
     ),
   );
 }
